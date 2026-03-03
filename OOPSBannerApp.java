e371bb61be143783b7cd5201deff1c2e4d3c450d
kp7135 <kp7135@srmist.edu>
2026-03-03 15:13:00 +0530

    UC6: Implemented static helper methods for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,29 +1,66 @@
 /**
- * OOPSBannerApp - UC5
- * Compact Array Initialization with String.join()
+ * OOPSBannerApp - UC6
+ * Using Static Helper Methods
  *
  * @author Karthik
- * @version 5.0
+ * @version 6.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Combined declaration + initialization
-        String[] banner = {
-                String.join("   ", " ***** ", " ***** ", " ******", " ***** "),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", "*     *", "*     *", "*      "),
-                String.join("   ", "*     *", "*     *", "****** ", " ***** "),
-                String.join("   ", "*     *", "*     *", "*      ", "      *"),
-                String.join("   ", "*     *", "*     *", "*      ", "*     *"),
-                String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
-        };
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
 
-        // Enhanced for-loop
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                    String.join("   ",
+                            o[i],
+                            o[i],
+                            p[i],
+                            s[i])
+            );
         }
+    }
+
+    // Static method for O
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Static method for P
+    public static String[] buildP() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Static method for S
+    public static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
     }
 }
